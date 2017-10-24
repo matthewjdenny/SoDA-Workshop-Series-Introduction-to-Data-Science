02ed155b81da093b9c8e8ae931f95fed5ce6d6af
Matt Denny <matthewjdenny@gmail.com>
2017-10-24 11:45:21 -0400

    materials for workshop 7

---
+++ Workshop_Scripts/Workshop_7-Additional_Functions.R
@@ -0,0 +1,64 @@
+# This function finds the row and column indicies of specified numbers in a
+# matrix, and returns an NA if the number could not be found. "mat" is the
+# input matrix, and numbers is a numeric vector of length one or more that
+# contains numbers we want to search for:
+find_positions <- function(mat,
+                           numbers) {
+
+
+    # Lets make sure the first argument is a matrix
+    if (class(mat)[1] != "matrix") {
+        stop("mat must be a matrix!")
+    }
+
+    # Check to make sure it is numeric
+    if (class(mat[1,1])[1] != "numeric" &
+        class(mat[1,1])[1] != "integer") {
+        stop("mat must be a numeric matrix!")
+    }
+
+    # Now lets make sure that numbers are numeric
+    if (class(numbers) != "numeric") {
+        stop("numbers must be a numeric vector")
+    }
+
+    # Create a three column data.frame to store the row and column indices (and
+    # the number itself), that we want to find.
+    indices <- data.frame(row = rep(NA,length(numbers)),
+                          column = rep(NA,length(numbers)),
+                          number = numbers)
+
+    # Loop over each number we want to find:
+    for (i in 1:length(numbers)) {
+
+        # We only want to find the first instance of a number, so we see a
+        # boolean to let ourselves know that we found it (or not).
+        Found <- FALSE
+
+        #Loop over rows and columns of the matrix:
+        for (j in 1:nrow(mat)) {
+            for (k in 1:ncol(mat)) {
+                if (mat[j,k] == numbers[i] & !Found) {
+                    indices[i,1] <- j # store row index
+                    indices[i,2] <- k # store column index
+                    Found <- TRUE # flip boolean
+                }
+            }
+        }
+    }
+
+    # Now just return results:
+    return(indices)
+}
+
+
+# Here is another toy function that says hello to the 'name' argument five
+# times, with a one second pause between each.
+say_hello <- function(name) {
+    for (i in 1:5) {
+        # Say hello:
+        cat("Hello ",name,"!\n", sep = "")
+        # Tells R to pause for one second:
+        Sys.sleep(1)
+    }
+}
---
+++ Workshop_Scripts/Workshop_7-Functions.R
@@ -0,0 +1,188 @@
+#### Functions ####
+
+
+### Basic Example Functions ###
+
+# User defined functions allow you to easily reuse a piece of code. These are
+# really the heart of R programming, and the basis for all R packages. Mastering
+# functions will allow you to enormously speed up your workflow and take on much
+# more complex projects.
+
+# Lets start with a really simple example
+first_function <- function(number) {
+    print(number)
+}
+
+# In our second example, we are going to define a function that will take the
+# sum of a particular column of a matrix (where the column index is a number):
+my_column_sum <- function(col_number,
+                          my_matrix) {
+
+    # Take the column sum of the matrix:
+    col_sum <- sum(my_matrix[,col_number])
+
+    # We always include a return statement at the end of the function which
+    # gives us back whatever the function computed. Note that we can only return
+    # one object (variable) in this return statement:
+    return(col_sum)
+}
+
+# Once we have defined our function, we simply enter it into the console. If all
+# went well, it will appear like nothing happened. However, the function will
+# now be available for us to use.
+
+# Lets try out our function on some example data:
+my_mat <- matrix(1:100,
+                 nrow = 10,
+                 ncol = 10)
+
+# Look at our matrix:
+my_mat
+
+# Now we use 'my_column_sum()' to take the sum of all elements in the first
+# column:
+temp <- my_column_sum(col_number = 1,
+                      my_matrix = my_mat)
+
+# Lets double check our result:
+sum(my_mat[,1])
+
+# Now we can loop through all columns in the matrix and apply our function to
+# each one. This is where the real power of functions comes out --that we can
+# use them inside of more complex programs, even inside other functions:
+for (i in 1:10) {
+    cat(my_column_sum(col_number = i,
+                      my_matrix = my_mat),"\n")
+}
+
+
+
+### A Function Template ###
+
+# Lets go over a general function template. We are also going to cover some
+# additional features of functions:
+
+template <- function (argument1,
+                      argument2 = 10, # default argument
+                      argument3 = NULL # NULL argument
+                      ) {
+
+    # It is often a good idea to check the types of variables you are passing
+    # in to functions. This can prevent you from introducing bugs into your
+    # code.
+
+    # For example, lets make sure the first argument is a matrix
+    if (class(argument1)[1] != "matrix") {
+        # the stop() function will halt execution if it is called.
+        stop("argument1 must be a matrix!")
+    }
+
+    # We may also want to check to make sure it is a numeric matrix. To do so
+    # we will need to check against both "numeric" and "integer" types in the
+    # first cell:
+    if (class(argument1[1,1])[1] != "numeric" &
+        class(argument1[1,1])[1] != "integer") {
+        # the stop() function will halt execution if it is called.
+        stop("argument1 must be a numeric matrix!")
+    }
+
+    # Now lets make sure that a single number is provided as argument2.
+    # First we make sure it is of length 1
+    if (length(argument2) != 1) {
+        stop("argument2 must be of length 1.")
+    }
+    # Now make sure it is numeric.
+    if (class(argument2)[1] != "numeric") {
+        stop("argument2 must be a number!")
+    }
+
+    # Check to see if argument 3 was NULL. If it was, do not do mroe stuff,
+    # but if it was not NULL, do more stuff.
+    do_more_stuff <- FALSE
+    if (!is.null(argument3)) {
+        do_more_stuff <- TRUE
+        if (class(argument3)[1] != "character") {
+            stop("argument3 must be a string, if it is provided.")
+        }
+    }
+
+    # Let the user know all variables checked out:
+    cat("All variables look good!\n")
+    # Now actually do stuff:
+
+    # Start by adding argument2 to each element of the matrix:
+    argument1 <- argument1 + argument2
+
+    # Now lets calcuate the sum:
+    total <- sum(argument1)
+
+    # If argument3 was provided:
+    if (do_more_stuff) {
+        # Visit each cell in the matrix
+        for (i in 1:nrow(argument1)) {
+            for (j in 1:ncol(argument1)) {
+                cat("The cell at [",i,",",j,"] contains ",argument1[i,j]," ",
+                    argument3,"s \n",sep = "")
+            }
+        }
+    }
+
+    # now return the updated matrix in argument1, and argument3 together in a
+    # list object
+    to_return <- list()
+    to_return$argument1 <- argument1
+    to_return$argument3 <- argument3
+
+    # We can only return one object, so a list can help work around this
+    # limitation:
+    return (to_return)
+
+}
+
+# Lets try it out!
+
+# Start by creating a matrix
+my_mat <- matrix(1:100,
+                 ncol = 10,
+                 nrow = 10)
+
+# We do not need to give an argument name if there is only one required
+# argument (one without a defaul value):
+template(my_mat)
+
+# Now lets change one of the default arguments
+template(my_mat,
+         argument2 = 20)
+
+# Finally lets change argument3
+template(my_mat,
+         argument2 = -5,
+         argument3 = "cat")
+
+
+
+### Using Functions Stored in Another File ###
+
+# Start by setting your working directory:
+setwd("~/Desktop")
+
+# Now use source() to load in additional user-defined functions:
+source("Additional_Functions.R")
+
+# Lets try out our function on some example data:
+my_mat <- matrix(1:900,
+                 nrow = 30,
+                 ncol = 30)
+
+# We need to pick some numbers to find:
+nums_to_find <- c(1,47,450,2000)
+
+# Now try out the function:
+find_positions(mat = my_mat,
+               numbers = nums_to_find)
+
+# And for some fun, lets say hello:
+say_hello("everyone")
+
+
+
